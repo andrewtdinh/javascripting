2ebe27d6c2db70bdea97ef1f91052c55f94d0ab0
Andrew Dinh <andrewdinh74@yahoo.com>
2015-09-06 22:55:44 -0700

    Second shot at implementing the reduce function

--- myRecursion.js
+++ myRecursion.js
@@ -1,10 +1,8 @@
 function reduce(arr, fn, initial) {
-  if (arr.length) {
-    var head = arr[0]
-    var tail = arr.slice[1]
-    return head + reduce(tail, fn, initial)
-  }
-  else return initial
+  if (!arr.length) return initial
+  var head = arr[0]
+  var tail = arr.slice[1]
+  return head + reduce(tail, fn, initial)
 }
 
 module.exports = reduce
